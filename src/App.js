ce369f941527035c90321aaf57ec846b7e7cc721
Mike Oram <Mikeoram01@gmail.com>
2022-11-08 11:11:26 +0000

    crated the calculator container component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import CalculatorContainer from "./Components/CalculatorContainer/CalculatorContainer";
 
 function App() {
   return (
     <div className="App">
+      <CalculatorContainer>
+
+      </CalculatorContainer>
     </div>
   );
 }
---
+++ src/Components/CalculatorContainer/CalculatorContainer.js
@@ -0,0 +1,9 @@
+import './CalculatorContainer.css'
+
+const CalculatorContainer = () => {
+    return (
+        <div className={"calculator-container"}></div>
+    )
+}
+
+export default  CalculatorContainer
