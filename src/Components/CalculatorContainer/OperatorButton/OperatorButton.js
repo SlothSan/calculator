550250814fe0f2a95e03145fadf126e28aab0b50
Mike Oram <Mikeoram01@gmail.com>
2022-11-11 11:02:19 +0000

    removed operator function - refactoring

--- src/Components/CalculatorContainer/OperatorButton/OperatorButton.js
+++ src/Components/CalculatorContainer/OperatorButton/OperatorButton.js
@@ -4,16 +4,7 @@ const OperatorButton = (props) => {
 
     const handleOperatorClick = (event) => {
         event.preventDefault()
-        if(props.displayValue.slice(-1) === props.operatorSymbol) {
-        } else {
-            if(props.operatorSymbol === '-') {
-                console.log(props.operatorSymbol)
-                props.setDisplayValue(String(props.displayValue + '-'))
-                props.setRunningValue(props.runningValue + '-')
-            }
-            props.setDisplayValue(String(props.displayValue + props.operatorSymbol))
-            props.setRunningValue(props.runningValue + props.operatorSymbol)
-        }
+        
         //TODO setup the logic for adding operators.
     }
 
